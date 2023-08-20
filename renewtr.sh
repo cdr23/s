d181712fbd709fbe6e876023c9dcc1f435ab303f
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-21 06:18:28 +0700

    Update renewtr.sh

--- renewtr.sh
+++ renewtr.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://worldssh.com/api/sc/akses.php | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-exit 0
-fi 
 
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/trojanws.json")
