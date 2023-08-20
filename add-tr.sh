f339b97f660dec85f514e4a698ec9e2c2f7f33f4
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-21 06:00:16 +0700

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -5,7 +5,7 @@ BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://worldssh.com/api/sc/akses.php | grep $MYIP )
+IZIN=$( curl https://github.com.com/cdr23/s/izin | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
