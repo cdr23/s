8ee3f22929d940191aa76a776e62c9e7a3199381
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-21 06:12:33 +0700

    Update member.sh

--- member.sh
+++ member.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://worldssh.com/api/sc/akses.php | grep $MYIP )
+IZIN=$( curl https://github.com/cdr23/s/izin | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
