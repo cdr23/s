b41848153597a97c5ebedbe44fd93b1377511e16
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-21 06:17:42 +0700

    Update renew-trg.sh

--- renew-trg.sh
+++ renew-trg.sh
@@ -16,14 +16,6 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://worldssh.tech/api/sc/akses.php | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-exit 0
-fi 
 
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/etc/xray/trojanws.json")
