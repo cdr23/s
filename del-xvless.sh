d47393ce97abbbe3f8d2c8d00d64adf2b385c479
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-21 06:36:05 +0700

    Update del-xvless.sh

--- del-xvless.sh
+++ del-xvless.sh
@@ -4,15 +4,6 @@ GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://worldssh.com/api/sc/akses.php | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-exit 0
-fi 
-
 
 clear             
 NUMBER_OF_CLIENTS=$(grep -c -E "^### Vless " "/etc/nginx/conf.d/vps.conf")
